0f9da503c9214110074cbe18a88d9bf9e0a8386e
keithkahurakamau <keithkahura@gmail.com>
2025-11-17 14:19:15 +0300

    added API and vercel settings

--- Hostel-Frontend/src/utils/api.js
+++ Hostel-Frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 // src/utils/api.js
-export const API_BASE_URL = 'http://localhost:5004';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5004';
 
 /**
  * Helper function to make API requests
