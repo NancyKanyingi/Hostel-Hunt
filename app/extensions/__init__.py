72ae47508c840f9d49e83e378569af3e0fa84b5d
Nancy Wangui Kanyingi <wangui.kanyingi2015@gmail.com>
2025-11-27 19:56:44 +0300

    removed unwanted app folder

--- app/extensions/__init__.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-from .jwt import jwt
-try:
-	from .mail import mail
-except Exception:
-	mail = None
-
-__all__ = ["db", "jwt", "mail"]
--- app/utils/jwt_utils.py
+++
@@ -1,7 +0,0 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
-
-def generate_tokens(identity):
-    return {
-        "access_token": create_access_token(identity=str(identity)),
-        "refresh_token": create_refresh_token(identity=str(identity)),
-    }
